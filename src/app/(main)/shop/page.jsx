082337348e7286e1002e378f637c4d7ad26e2f9d
Redwan Hasan <redwanhasan.workspace@gmail.com>
2026-04-22 11:05:30 +0600

    feat: integrate ShopTabs component into shop page layout
    - Replaced the welcome message and product exploration message with the ShopTabs component for improved navigation and user experience.

--- src/app/(main)/shop/page.jsx
+++ src/app/(main)/shop/page.jsx
@@ -1,10 +1,13 @@
+import ShopTabs from "@/components/shop/ShopTabs";
+
 const page = () => {
-    return (
-        <div>
-          <h1 className="text-3xl font-bold text-center mt-10">Welcome to the Shop Page</h1>
-          <p className="text-center mt-4 text-gray-600">Explore our products and find what you love!</p>  
+  return (
+    <div>
+        <div className="container mx-auto py-10">
+            <ShopTabs/>
         </div>
-    );
+    </div>
+  );
 };
 
 export default page;
---
+++ src/components/shop/ShopTabs.jsx
@@ -0,0 +1,64 @@
+"use client";
+import { SlidersHorizontal } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+const ShopTabs = () => {
+  const [activeTab, setActiveTab] = useState("All Products");
+  const tabs = [
+    {
+      id: 1,
+      name: "All Products",
+    },
+    {
+      id: 2,
+      name: "T-shirts",
+    },
+    {
+      id: 3,
+      name: "Jachets",
+    },
+    {
+      id: 4,
+      name: "Shoes",
+    },
+  ];
+  return (
+    <div className="flex flex-col md:flex-row gap-4 md:gap-0 md:px-8 justify-between items-center">
+      <div className="flex gap-4 flex-wrap">
+        {tabs.map((tab) => (
+          <Link
+            href={`/shop?tab=${tab.name}`}
+            onClick={() => setActiveTab(tab.name)}
+            key={tab.id}
+            className={`hover:text-indigo-600 text-gray-500 font-medium border-b-2 border-transparent hover:border-indigo-600 transition-all duration-300 ${activeTab === tab.name ? "border-indigo-600 text-indigo-600" : ""}`}
+          >
+            {tab.name}
+          </Link>
+        ))}
+      </div>
+      <div className="dropdown dropdown-end">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn btn-ghost border-2 rounded-full border-indigo-500 text-indigo-500 flex items-center gap-2 hover:bg-white hover:border-indigo-600 hover:text-indigo-600 transition-all duration-300"
+        >
+          <SlidersHorizontal size={20} />
+          <span className="text-sm font-medium">Filter</span>
+        </div>
+        <ul
+          tabIndex="-1"
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li>
+            <a>Price: Low to High</a>
+          </li>
+          <li>
+            <a>Price: High to Low</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ShopTabs;
