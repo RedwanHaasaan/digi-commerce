fbca697af4e68c7b4cce3b034dc647e06c7d885c
Redwan Hasan <redwanhasan.workspace@gmail.com>
2026-04-29 17:47:13 +0600

    Refactor: update cart page layout and enhance styling
    - Added Breadcrumbs component for improved navigation.
    - Simplified cart page structure by removing unnecessary divs and adjusting padding.
    - Enhanced CartButton styling for better visual appeal and responsiveness.
    - Removed CouponCode and OrderSummary components to streamline the cart experience.

--- src/app/(main)/shop/cart/page.jsx
+++ src/app/(main)/shop/cart/page.jsx
@@ -1,22 +1,24 @@
 "use client";
+import Breadcrumbs from "@/components/Breadcrumb/Breadcrumb";
 import CartButton from "@/components/shop/cart/CartButton";
 import CartItem from "@/components/shop/cart/CartItem";
-import CouponCode from "@/components/shop/cart/CouponCode";
 import NoProduct from "@/components/shop/cart/NoProduct";
-import OrderSummery from "@/components/shop/cart/OrderSummery";
 import useCartDetails from "@/hooks/useCartDetails";
 
 const Page = () => {
   const { cart, cartWithDetails } = useCartDetails();
   return (
-    <div className="bg-base-200 min-h-105">
-      <div className="container mx-auto py-10 px-2 flex flex-col lg:flex-row gap-10">
-        <div className="bg-base-100 rounded-lg p-4 shadow-md flex flex-col gap-4 flex-1">
-          <h1 className="text-2xl font-semibold border-b pb-4 border-b-gray-300">
+    <div className="bg-base-200 min-h-105 py-10">
+      <div className="container mx-auto px-2">
+        <Breadcrumbs/>
+      </div>
+      <div className="container mx-auto px-2">
+        <div className="bg-base-100 rounded-lg p-4 shadow-md">
+          <h1 className="text-2xl font-semibold pb-4 border-b-gray-300">
             Your Cart
           </h1>
           {/* CartItem */}
-          <div className="flex flex-col gap-4 xl:max-h-105 overflow-y-auto no-scrollbar">
+          <div className="flex flex-col gap-4 xl:max-h-105 overflow-y-auto mt-4">
             {cart.length > 0 ? (
               cartWithDetails.map((cartItem) => (
                 <CartItem key={cartItem.id} cartItem={cartItem} />
@@ -25,18 +27,8 @@ const Page = () => {
               <NoProduct />
             )}
           </div>
-        </div>
-        <div className="flex flex-col md:flex-row lg:flex-col w-full lg:w-xs gap-5">
-          {/* Coupon code */}
-            <CouponCode/>
-          {/* Order Summery */}
-          <div className="flex flex-col bg-white rounded-lg p-4 w-full gap-5">
-            <h1 className="text-2xl font-semibold border-b pb-4 border-b-gray-300">
-              Order Summery
-            </h1>
-            <OrderSummery items={cartWithDetails} />
-            {/* Go to checkout */}
-            <CartButton text="Checkout" path="/checkout"/>
+          <div className="border-t border-gray-200 mt-6">
+            <CartButton classname="flex-row-reverse justify-between items-center" btnCls="group px-8 py-4 rounded-xl bg-linear-to-r from-indigo-600 to-purple-600 text-white font-bold shadow-lg hover:shadow-2xl hover:from-indigo-700 hover:to-purple-700 active:scale-95 transition-all duration-300" text="Checkout" path="/shop/cart/checkout"/>
           </div>
         </div>
       </div>
